8ce1529d83510ec830a4474a8a95e3b2791e8471
BMWSawyer <bradley.mw.sawyer@gmail.com>
2022-03-24 10:20:58 -0700

    Completed the listing details screen. Added a list item component

--- App.js
+++ App.js
@@ -16,6 +16,7 @@ import WelcomeScreen from "./app/screens/WelcomeScreen";
 import AppText from "./app/components/AppText";
 import AppButton from "./app/components/AppButton";
 import Card from "./app/components/Card";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 
 export default function App() {
   return (
@@ -26,13 +27,14 @@ export default function App() {
     //   </View>
     // </SafeAreaView>
     // <WelcomeScreen />
-    <View style={{ backgroundColor: "#f8f4f4", padding: 20, paddingTop: 100 }}>
-      <Card
-        title="Salmon"
-        subTitle="$100"
-        image={require("./app/assets/main-course.jpg")}
-      />
-    </View>
+    // <View style={{ backgroundColor: "#f8f4f4", padding: 20, paddingTop: 100 }}>
+    //   <Card
+    //     title="Salmon"
+    //     subTitle="$100"
+    //     image={require("./app/assets/main-course.jpg")}
+    //   />
+    // </View>
+    <ListingDetailsScreen />
   );
 }
 
--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -1,17 +1,47 @@
 import React from "react";
 import { View, Image, Text, StyleSheet } from "react-native";
+import AppText from "../components/AppText";
+import colors from "../config/colors";
+import ListItem from "../components/ListItem";
 
 export default function ListingDetailsScreen(props) {
   return (
     <View>
       <Image style={styles.image} source={require("../assets/breakfast.jpg")} />
+      <View style={styles.detailsContainer}>
+        <AppText style={styles.title}>French Toast</AppText>
+        <AppText style={styles.price}>$100</AppText>
+        <View style={styles.userContainer}>
+          <ListItem
+            image={require("../assets/profilepic.jpg")}
+            title="John Smith"
+            subTitle="5 Listings"
+          />
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  detailsContainer: {
+    padding: 20,
+  },
   image: {
     width: "100%",
     height: 300,
+  },
+  price: {
+    color: colors.secondary,
+    fontWeight: "bold",
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "500",
+  },
+  userContainer: {
+    marginVertical: 40,
   },
 });
