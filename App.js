5382925b3e3b02a88378fad6279e03b206cf5c96
BMWSawyer <bradley.mw.sawyer@gmail.com>
2022-03-28 11:40:09 -0700

    Updated to see screen while implementing the ListItem component

--- App.js
+++ App.js
@@ -2,12 +2,13 @@ import React from "react";
 //import { StatusBar } from "expo-status-bar";
 import { View } from "react-native";
 import Icon from "./app/components/Icon";
+import ListItem from "./app/components/ListItem";
 import Screen from "./app/components/Screen";
 
 export default function App() {
   return (
     <Screen>
-      <Icon name="email" size={100} />
+      <ListItem title="My title" ImageComponent={<Icon name="email" />} />
     </Screen>
   );
 }
