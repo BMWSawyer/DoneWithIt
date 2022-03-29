9ceaa96f803319ca77aec470cdc611a446ca22c0
BMWSawyer <bradley.mw.sawyer@gmail.com>
2022-03-29 16:17:16 -0700

    Implemented ability to handle selections in the picker list

--- App.js
+++ App.js
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import AppPicker from "./app/components/AppPicker";
 import AppTextInput from "./app/components/AppTextInput";
 //import { StatusBar } from "expo-status-bar";
 import Screen from "./app/components/Screen";
 
+const categories = [
+  { label: "Dinner", value: 1 },
+  { label: "Lunch", value: 2 },
+  { label: "Breakfast", value: 3 },
+];
+
 export default function App() {
-  const categories = [
-    { label: "Dinner", value: 1 },
-    { label: "Lunch", value: 2 },
-    { label: "Breakfast", value: 3 },
-  ];
+  const [category, setCategory] = useState(categories[0]);
 
   return (
     <Screen>
-      <AppPicker items={categories} icon="apps" placeholder="Category" />
+      <AppPicker
+        selectedItem={category}
+        onSelectItem={(item) => setCategory(item)}
+        items={categories}
+        icon="apps"
+        placeholder="Category"
+      />
       <AppTextInput icon="email" placeholder="email" />
     </Screen>
   );
