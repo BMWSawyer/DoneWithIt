7fb50636130b664e9c38e9e9572544b16cfb6dd3
BMWSawyer <bradley.mw.sawyer@gmail.com>
2022-03-30 11:30:30 -0700

    Built the login screen with a login form

--- App.js
+++ App.js
@@ -1,28 +1,7 @@
 import React, { useState } from "react";
-import AppPicker from "./app/components/AppPicker";
-import AppTextInput from "./app/components/AppTextInput";
+import LoginScreen from "./app/screens/LoginScreen";
 //import { StatusBar } from "expo-status-bar";
-import Screen from "./app/components/Screen";
-
-const categories = [
-  { label: "Dinner", value: 1 },
-  { label: "Lunch", value: 2 },
-  { label: "Breakfast", value: 3 },
-];
 
 export default function App() {
-  const [category, setCategory] = useState(categories[0]);
-
-  return (
-    <Screen>
-      <AppPicker
-        selectedItem={category}
-        onSelectItem={(item) => setCategory(item)}
-        items={categories}
-        icon="apps"
-        placeholder="Category"
-      />
-      <AppTextInput icon="email" placeholder="email" />
-    </Screen>
-  );
+  return <LoginScreen />;
 }
