0bd6c0ad0fd60e47a123b778e63c8e3872d3dcc0
BMWSawyer <bradley.mw.sawyer@gmail.com>
2022-04-04 10:28:14 -0700

    Added permission dependencies. Started logic to access users media library

--- App.js
+++ App.js
@@ -1,8 +1,21 @@
-import React from "react";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+import React, { useEffect } from "react";
+import * as ImagePicker from "expo-image-picker";
+import * as Permissions from "expo-permissions";
+
+import Screen from "./app/components/Screen";
 
 //import { StatusBar } from "expo-status-bar";
 
 export default function App() {
-  return <ListingEditScreen />;
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (!granted)
+      alert("You need to enable permissions to access the library.");
+  };
+
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
+  return <Screen></Screen>;
 }
