6d3d5b1bf67aaa9512f41234a53903a368ea7608
BMWSawyer <bradley.mw.sawyer@gmail.com>
2022-04-05 09:19:27 -0700

    Built the logic for accessing the users media library

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { Button, Image } from "react-native-web";
 import * as ImagePicker from "expo-image-picker";
 import * as Permissions from "expo-permissions";
 
@@ -7,6 +8,8 @@ import Screen from "./app/components/Screen";
 //import { StatusBar } from "expo-status-bar";
 
 export default function App() {
+  const [imageUri, setImageUri] = useState();
+
   const requestPermission = async () => {
     const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
     if (!granted)
@@ -17,5 +20,19 @@ export default function App() {
     requestPermission();
   }, []);
 
-  return <Screen></Screen>;
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.cancelled) setImageUri(result.uri);
+    } catch (error) {
+      console.log("Error reading an image", error);
+    }
+  };
+
+  return (
+    <Screen>
+      <Button title="Select Image" onPress={selectImage} />
+      <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
+    </Screen>
+  );
 }
